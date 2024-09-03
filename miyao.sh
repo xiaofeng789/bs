9b13deca24016318c6834d41ba466cc077022d3f
xiaofeng789 <y6y6y6y666@gmail.com>
2024-09-03 10:20:18 +0800

    2024.9.3

---
+++ miyao.sh
@@ -0,0 +1,6 @@
+git push
+git remote set-url origin git@github.com:xiaofeng789/bs.git
+git init
+cat ~/.ssh/id_ed25519.pub
+ssh-add ~/.ssh/id_ed25519
+ssh-keygen -t ed25519 -C "y6y6y6y666@gmail.com"
